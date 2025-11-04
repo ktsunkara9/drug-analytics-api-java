7fc029aeda64c76191ca594f1999e9d62b46ae85
Krishna Teja Sunkara <ktsunkara@gmail.com>
2025-11-04 23:19:26 +0530

    initial commit - spring boot skeleton for Drug Analytics API

---
+++ src/main/java/com/druganalytics/api/DrugAnalyticsApiApplication.java
@@ -0,0 +1,13 @@
+package com.druganalytics.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrugAnalyticsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrugAnalyticsApiApplication.class, args);
+	}
+
+}
