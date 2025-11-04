3c08d4d01ab15ec5a9edbd4275b30435c35de6ff
Krishna Teja Sunkara <ktsunkara@gmail.com>
2025-11-05 00:28:39 +0530

    added basic controller and dev profile

---
+++ src/main/java/com/druganalytics/api/controller/UploadController.java
@@ -0,0 +1,15 @@
+package com.druganalytics.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class UploadController {
+
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "Hello from Drug Analytics API!";
+	}
+}
